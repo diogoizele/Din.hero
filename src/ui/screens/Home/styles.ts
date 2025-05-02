1ff965ec7b886cbfc10f1b902c886ea0ce7cdc77
diogoizele <diogogabrielizele@gmail.com>
2025-05-02 19:14:37 -0300

    feat: create home screen

---
+++ src/ui/screens/Home/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+import { Colors } from 'react-native-ui-lib';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'relative',
+    backgroundColor: Colors.background,
+  },
+});

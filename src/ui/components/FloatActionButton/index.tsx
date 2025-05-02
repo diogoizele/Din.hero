6b710a4b5c94835c0c86aa9fa140e43a26102234
diogoizele <diogogabrielizele@gmail.com>
2025-05-02 19:14:54 -0300

    feat: create FAB component

---
+++ src/ui/components/FloatActionButton/index.tsx
@@ -0,0 +1,25 @@
+import { Button } from 'react-native-ui-lib';
+
+import Icon from '../Icon';
+import { styles } from './styles';
+import { iconObjectMapper } from '../Icon/icon-object-mapper';
+import { useTheme } from '../../../providers/ThemeProvider';
+
+interface Props {
+  icon: keyof typeof iconObjectMapper;
+}
+
+function FloatActionButton({ icon }: Props) {
+  const { borderRadiuses } = useTheme();
+
+  return (
+    <Button
+      style={styles.container}
+      round
+      borderRadius={borderRadiuses.brRound}>
+      <Icon name={icon} />
+    </Button>
+  );
+}
+
+export default FloatActionButton;
---
+++ src/ui/components/FloatActionButton/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    bottom: 36,
+    right: 24,
+    width: 64,
+    height: 64,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});

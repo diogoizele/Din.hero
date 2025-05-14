bd7ccd390f553952cc072dc891e734269c9bc5cb
diogoizele <diogogabrielizele@gmail.com>
2025-05-13 21:52:13 -0300

    feat(Icons): add arrow-left and close icon

--- src/presentation/components/Icon/icon-object-mapper.ts
+++ src/presentation/components/Icon/icon-object-mapper.ts
@@ -1,5 +1,9 @@
 import Plus from './icons/Plus.svg';
+import ArrowLeft from './icons/Arrow-Left.svg';
+import Close from './icons/Close.svg';
 
 export const iconObjectMapper = {
   plus: { component: Plus, colorProp: 'stroke' },
+  'arrow-left': { component: ArrowLeft, colorProp: 'fill' },
+  close: { component: Close, colorProp: 'fill' },
 };

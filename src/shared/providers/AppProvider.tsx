12b1970b5e7da9791b640852f6d7b498b0e13489
diogoizele <diogogabrielizele@gmail.com>
2025-05-13 21:54:36 -0300

    feat: add react-hook-form provider

--- src/shared/providers/AppProvider.tsx
+++ src/shared/providers/AppProvider.tsx
@@ -4,18 +4,23 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import ThemeProvider from './ThemeProvider';
+import { FormProvider, useForm } from 'react-hook-form';
 
 type Props = {
   children: ReactNode;
 };
 
 function AppProvider({ children }: Props) {
+  const methods = useForm();
+
   return (
     <GestureHandlerRootView>
       <ThemeProvider>
-        <SafeAreaProvider>
-          <NavigationContainer>{children}</NavigationContainer>
-        </SafeAreaProvider>
+        <FormProvider {...methods}>
+          <SafeAreaProvider>
+            <NavigationContainer>{children}</NavigationContainer>
+          </SafeAreaProvider>
+        </FormProvider>
       </ThemeProvider>
     </GestureHandlerRootView>
   );

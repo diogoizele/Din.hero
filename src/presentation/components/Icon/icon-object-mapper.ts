44765385b9cf100b6510ca551aa0f14c2f99fc10
diogoizele <diogogabrielizele@gmail.com>
2025-05-21 21:55:59 -0300

    feat: add new calendar icon

--- src/presentation/components/Icon/icon-object-mapper.ts
+++ src/presentation/components/Icon/icon-object-mapper.ts
@@ -1,9 +1,11 @@
 import Plus from './icons/Plus.svg';
 import ArrowLeft from './icons/Arrow-Left.svg';
 import Close from './icons/Close.svg';
+import Calendar from './icons/Calendar.svg';
 
 export const iconObjectMapper = {
   plus: { component: Plus, colorProp: 'stroke' },
   'arrow-left': { component: ArrowLeft, colorProp: 'fill' },
   close: { component: Close, colorProp: 'fill' },
+  calendar: { component: Calendar, colorProp: 'fill' },
 };

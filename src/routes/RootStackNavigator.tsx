a1bdb371627008465d0bb32acb09151684b512d2
diogoizele <diogogabrielizele@gmail.com>
2025-10-25 09:15:49 -0300

    feat(RootStackNavigator): add History screen to the stack navigator with default header options

--- src/routes/RootStackNavigator.tsx
+++ src/routes/RootStackNavigator.tsx
@@ -3,28 +3,39 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import RegisterBill from '../screens/RegisterBill/RegisterBillView';
 import { NavigationProp } from '@react-navigation/native';
 import TabNavigator from './TabNavigator';
+import History from '../screens/History/HistoryView';
 
 export type RootStackParamList = {
   Home: undefined;
   RegisterBill: undefined;
+  History: undefined;
 };
 
 export type NavigationProps = Omit<NavigationProp<RootStackParamList>, 'state'>;
 
 const StackNavigator = createNativeStackNavigator<RootStackParamList>();
 
+const defaultScreenOptions = {
+  headerShown: false,
+};
+
 function RootStackNavigator() {
   return (
     <StackNavigator.Navigator>
       <StackNavigator.Screen
         name="Home"
-        options={{ headerShown: false }}
+        options={defaultScreenOptions}
         component={TabNavigator}
       />
       <StackNavigator.Screen
         name="RegisterBill"
-        options={{ headerShown: false }}
+        options={defaultScreenOptions}
         component={RegisterBill}
+      />
+      <StackNavigator.Screen
+        name="History"
+        options={defaultScreenOptions}
+        component={History}
       />
     </StackNavigator.Navigator>
   );
